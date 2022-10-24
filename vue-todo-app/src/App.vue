8ae2d37aba98569af444d286d9e46616b3d8580c
Sasen Perera <sasen.learnings@gmail.com>
2022-10-24 19:35:25 +0530

    Created Vue's Todo-App

---
+++ vue-todo-app/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import TodoComponent from './components/TodoComponent.vue';
+</script>
+
+<template>
+  <header>
+    <h1 :class="title">Todo App</h1>
+  </header>
+
+  <main>
+    <TodoComponent />
+  </main>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  background-color: coral;
+}
+
+.title {
+  color: black;
+  text-align: center;
+  line-height: 2.0;
+}
+</style>
---
+++ vue-todo-app/src/components/TodoComponent.vue
@@ -0,0 +1,43 @@
+<script>
+    export default {
+        newTodo : '',
+        data(){
+            return {
+                todos: [
+                    {
+                        title: 'Learn Vue'
+                    },
+                    {
+                        title: 'Learn React'
+                    },
+                    {
+                        title: 'Learn Angular'
+                    }
+                ]
+            }
+        },
+        methods: {
+            addTodo(){
+                this.todos.push({
+                    title: this.newTodo
+                })
+            },
+            removeTodo(id){
+                this.todos.splice(id, 1)
+            }
+        }
+    };
+</script>
+
+<template>
+    <div>
+        <ul>
+            <li v-for="(todo, index) in todos" :key="index">
+                {{ todo.title }}
+                <input type="checkbox" @click="makecross" />
+                <button @click="removeTodo">X</button>
+            </li>
+        </ul>
+        <input type="text" v-model="newTodo" @keyup.enter="addTodo">
+    </div>
+</template>
